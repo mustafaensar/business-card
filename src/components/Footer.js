de8f2ad708ddf11b1a946ff485de6e9ccc5bdeb9
mustafaensar <mustafaensar55@gmail.com>
2023-02-26 21:09:25 +0100

    links are able to open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,12 +11,21 @@ function Footer(){
   return(
     <div className="footer">
       <div className="icons">
+        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/lawyermstf">
         <FontAwesomeIcon icon={faTwitter} className="footer-icon"></FontAwesomeIcon>
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/musenscek">
         <FontAwesomeIcon icon={faFacebook} className="footer-icon"></FontAwesomeIcon>
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/lawyermstf/">
         <FontAwesomeIcon icon={faInstagram} className="footer-icon"></FontAwesomeIcon>
-        <FontAwesomeIcon icon={faInstagram} className="footer-icon"></FontAwesomeIcon>
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mustafaensarcekic/">
         <FontAwesomeIcon icon={faLinkedin} className="footer-icon"></FontAwesomeIcon>
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/mustafaensar">
         <FontAwesomeIcon icon={faGithub} className="footer-icon"></FontAwesomeIcon>
+        </a>
       </div>
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(){
     <div className="header">
       <h2>Mustafa Ensar Cekic</h2>
       <h4>Blockchain Developer</h4>
-      <a href="https://github.com/mustafaensar" className="header-link">mustafaensar.website</a>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/mustafaensar" className="header-link">mustafaensar.website</a>
       <button><i className="fa button-icon">&#xf0e0;</i> Email</button>
     </div>
   )

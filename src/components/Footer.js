0a7695cad2c90f899eb73fd2ccf1fefc1e3bada4
mustafaensar <mustafaensar55@gmail.com>
2023-02-26 15:45:44 +0100

    icons added from react-fontawesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,22 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faTwitter} from "@fortawesome/free-brands-svg-icons"
+import {faFacebook} from "@fortawesome/free-brands-svg-icons"
+import {faInstagram} from "@fortawesome/free-brands-svg-icons"
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
+import {faGithub} from "@fortawesome/free-brands-svg-icons"
 import "./Footer.css"
 
 function Footer(){
   return(
     <div className="footer">
       <div className="icons">
-        <a href="https://twitter.com/lawyermstf" className="fa fa-twitter footer-icon"> </a>
-        <a href="https://twitter.com/lawyermstf" className="fa fa-facebook footer-icon"> </a>
-        <a href="https://twitter.com/lawyermstf" className="fa fa-instagram footer-icon"> </a>
-        <a href="https://twitter.com/lawyermstf" className="fa fa-linkedin footer-icon"> </a>
-        <a href="https://twitter.com/lawyermstf" className="fa fa-github footer-icon"> </a>
+        <FontAwesomeIcon icon={faTwitter} className="footer-icon"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faFacebook} className="footer-icon"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faInstagram} className="footer-icon"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faInstagram} className="footer-icon"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faLinkedin} className="footer-icon"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faGithub} className="footer-icon"></FontAwesomeIcon>
       </div>
     </div>
   )
